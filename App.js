fecc85b12ff79b86fdac4bcc08a6bb7ab09f5727
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2019-07-05 15:41:17 -0300

    link react-native-vector-icons

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import FabricIcons from 'react-native-vector-icons/FabricIcons';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +11,16 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <FabricIcons name="Add" size={30} color="#900" />
+        <FabricIcons name="Cancel" size={30} color="#090" />
+        <FabricIcons name="More" size={30} color="#009" />
       </View>
     );
   }
